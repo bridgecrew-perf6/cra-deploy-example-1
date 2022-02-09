5eed1d23ba6a2cfbd38a9456aafc414d387e923f
przekwas <luke@covalence.io>
2022-02-08 23:32:18 -0600

    adding sick feature

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I done did lern sum React !  ;)
         </a>
       </header>
     </div>
